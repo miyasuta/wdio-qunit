10a4f071ddf572047b1bc914ff01c786c41c9148
Mio Yasutake <mio.fujita.01@gmail.com>
2023-12-14 05:32:15 +0900

    add tests

---
+++ webapp/test/integration/integrationTests.test.js
@@ -0,0 +1,9 @@
+describe("QUnit test page", () => {
+    it("should pass Integration tests", async () => {
+        await browser.url(
+            "http://localhost:8080/test/integration/opaTests.qunit.html"
+        );
+        const qunitResults = await browser.getQUnitResults();
+        expect(qunitResults).toBeTruthy();
+    });
+});
